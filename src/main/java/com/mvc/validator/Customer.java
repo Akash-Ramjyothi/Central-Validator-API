ff992c70694ba8eae4dc6ad696ff03e173580eb4
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-11-12 01:30:19 +0530

    Validated freePasses field

--- src/main/java/com/mvc/validator/Customer.java
+++ src/main/java/com/mvc/validator/Customer.java
@@ -9,9 +9,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName = "";
 
-    @Min(value = 0, message = "must be freater than or equal to 0")
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to 0")
     @Max(value = 10, message = "must be lesser than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -32,11 +33,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
